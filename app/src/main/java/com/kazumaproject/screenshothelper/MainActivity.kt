2aa75c207074f618897ca592d8f09ba4e28715e9
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2021-03-17 12:11:52 -0400

    Create layout

--- app/src/main/java/com/kazumaproject/screenshothelper/MainActivity.kt
+++ app/src/main/java/com/kazumaproject/screenshothelper/MainActivity.kt
@@ -2,12 +2,23 @@ package com.kazumaproject.screenshothelper
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        CoroutineScope(Dispatchers.Default).launch {
+
+        }
     }
+
 }
---
+++ app/src/main/java/com/kazumaproject/screenshothelper/di/Application.kt
@@ -0,0 +1,7 @@
+package com.kazumaproject.screenshothelper.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application: Application()
